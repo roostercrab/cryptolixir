34e8b490b5b8a6328bc4b86c4c39c5fd36139082
roostercrab <jesse.shea@qixingdesign.com>
2020-08-08 17:25:55 -0700

    large numbers start producing consistent results

--- lib/cryptolixir/entropy_test.ex
+++ lib/cryptolixir/entropy_test.ex
@@ -1,25 +1,33 @@
 defmodule EntropyTest do
-  def entropy_introducer(keyboard_list, password, 0) do
-    entropy = Enum.random(128..999_999_999_999_999_999) * Enum.random(128..999_999_999_999_999_999)
-
-    for character <- password, do: calc_entropy(keyboard_list, entropy, character)
-  end
-
   def entropy_introducer(keyboard_list, password, user_entropy) do
-    entropy = user_entropy * Enum.random(128..999_999_999_999_999_999)
-
-    for character <- password, do: calc_entropy(keyboard_list, entropy, character)
+    for character <- password, do: calc_entropy(keyboard_list, character, user_entropy)
   end
 
-  defp calc_entropy(keyboard_list, entropy, character) do
-    # stringed_character = to_string(character)
-    # <<character_number, 0>> = stringed_character <> <<0>>
-
+  defp calc_entropy(keyboard_list, character, user_entropy) do
     keyboard_list_length = Enum.count(keyboard_list)
 
-    entropy_position = rem((entropy * character), keyboard_list_length)
-    IO.puts(entropy_position)
-    IO.puts(is_number(entropy_position))
+    IO.inspect(user_entropy, label: "User Entropy")
+
+    random_number = Enum.random(character..999_999_999)
+    IO.inspect(random_number, label: "Random Number")
+
+    IO.inspect(character, label: "Character")
+
+    entropy = (pow(random_number, character) |> round) * user_entropy
+    IO.inspect(entropy, label: "User Entropy * Random Number * Character")
+
+    entropy_position = rem(entropy, keyboard_list_length - 1)
+    IO.inspect(entropy_position, label: "Modulus Entropy Times Character to Keyboard Length -1")
+
     Enum.at(keyboard_list, entropy_position)
   end
+
+
+    defp pow(_, 0), do: 1
+    defp pow(x, n) when rem(n, 2) == 1, do: x * pow(x, n - 1)
+
+    defp pow(x, n) do
+      result = pow(x, div(n, 2))
+      result * result
+    end
 end
--- lib/cryptolixir/password_creator.ex
+++ lib/cryptolixir/password_creator.ex
@@ -1,41 +1,46 @@
 defmodule Cryptolixir.PasswordCreator do
- def return_password(options, length, user_entropy) do
-   keyboard_list = parse_options(options, [])
-   password = extract_random(keyboard_list, length, [])
-   entropy_introducer(keyboard_list, password, user_entropy)
- end
+  def return_password(options, length) do
+    keyboard_list = parse_options(options, [])
+    extract_random(keyboard_list, length, [])
+  end
 
- defp parse_options([], include_list) do
-   List.flatten(include_list)
- end
+  def return_password(options, length, user_entropy) do
+    keyboard_list = parse_options(options, [])
+    password = extract_random(keyboard_list, length, [])
+    entropy_introducer(keyboard_list, password, user_entropy)
+  end
 
- defp parse_options([head_options | tail_options], include_list) do
-   lowercase = ~c(abcdefghijklmnopqrstuvwxyz)
-   uppercase = ~c(ABCDEFGHIJKLMNOPQRSTUVWXYZ)
-   numbers = ~c(1234567890)
-   special = ~c(`-=~!@#$%^&*\(\)_+\[\]\{\}|;':"<>?,./\\)
+  defp parse_options([], include_list) do
+    List.flatten(include_list)
+  end
 
-   add_to_include_list =
-     case head_options do
-       "lowercase" -> lowercase
-       "uppercase" -> uppercase
-       "numbers" -> numbers
-       "special" -> special
-     end
+  defp parse_options([head_options | tail_options], include_list) do
+    lowercase = ~c(abcdefghijklmnopqrstuvwxyz)
+    uppercase = ~c(ABCDEFGHIJKLMNOPQRSTUVWXYZ)
+    numbers = ~c(1234567890)
+    special = ~c(`-=~!@#$%^&*\(\)_+\[\]\{\}|;':"<>?,./\\)
 
-   new_include_list = [add_to_include_list | include_list]
+    add_to_include_list =
+      case head_options do
+        "lowercase" -> lowercase
+        "uppercase" -> uppercase
+        "numbers" -> numbers
+        "special" -> special
+      end
 
-   parse_options(tail_options, new_include_list)
- end
+    new_include_list = [add_to_include_list | include_list]
 
- defp extract_random(_keyboard_list, 0, character_list) do
-   character_list
- end
+    parse_options(tail_options, new_include_list)
+  end
 
- defp extract_random(keyboard_list, length, character_list) do
-   shuffled = Enum.shuffle(keyboard_list)
-   random_character = Enum.random(shuffled)
-   added_character_list = [random_character | character_list]
-   extract_random(shuffled, length - 1, added_character_list)
- end
+  defp extract_random(_keyboard_list, 0, character_list) do
+    character_list
+  end
+
+  defp extract_random(keyboard_list, length, character_list) do
+    shuffled = Enum.shuffle(keyboard_list)
+    random_character = Enum.random(shuffled)
+    added_character_list = [random_character | character_list]
+    extract_random(shuffled, length - 1, added_character_list)
+  end
 end
