f2c2a20539bbe4834a9080a1f504bc96bfe01293
roostercrab <jesse.shea@qixingdesign.com>
2020-08-08 18:21:51 -0700

    maybe we don't pass in a password..

--- lib/cryptolixir/entropy_test.ex
+++ lib/cryptolixir/entropy_test.ex
@@ -5,29 +5,23 @@ defmodule EntropyTest do
 
   defp calc_entropy(keyboard_list, character, user_entropy) do
     keyboard_list_length = Enum.count(keyboard_list)
+    entropy = pow(user_entropy, keyboard_list_length)
 
-    IO.inspect(user_entropy, label: "User Entropy")
-
-    random_number = Enum.random(character..999_999_999)
-    IO.inspect(random_number, label: "Random Number")
+    IO.inspect(entropy, label: "Entropy")
 
     IO.inspect(character, label: "Character")
 
-    entropy = (pow(random_number, character) |> round) * user_entropy
-    IO.inspect(entropy, label: "User Entropy * Random Number * Character")
-
-    entropy_position = rem(entropy, keyboard_list_length - 1)
-    IO.inspect(entropy_position, label: "Modulus Entropy Times Character to Keyboard Length -1")
+    entropy_position =
+    IO.inspect(entropy_position, label: "Entropy Position")
 
     Enum.at(keyboard_list, entropy_position)
   end
 
+  defp pow(_, 0), do: 1
+  defp pow(x, n) when rem(n, 2) == 1, do: x * pow(x, n - 1)
+  defp pow(x, n) do
+    result = pow(x, div(n, 2))
+    result * result
+  end
 
-    defp pow(_, 0), do: 1
-    defp pow(x, n) when rem(n, 2) == 1, do: x * pow(x, n - 1)
-
-    defp pow(x, n) do
-      result = pow(x, div(n, 2))
-      result * result
-    end
 end
